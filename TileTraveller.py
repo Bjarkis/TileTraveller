642b3c46fce1096bb401ca67a044b1aeae9bad14
Bjarki Sigurðsson <bjarki@absalon.is>
2020-09-17 10:56:45 +0000

    Complete

--- TileTraveller.py
+++ TileTraveller.py
@@ -25,7 +25,7 @@ while room != "31":
         direction = input("You can travel: (E)ast or (S)outh. ")
         if direction.lower() == "e":
             print("Direction: " + direction)
-            room = "13"
+            room = "23"
         elif direction.lower() == "s":
             print("Direction: " + direction)
             room = "11"
@@ -48,3 +48,35 @@ while room != "31":
             room = "12"
         else:
             print("Not a valid direction!")
+    elif room == "23":
+        direction = input("You can travel: (E)ast or (W)est. ") 
+        if direction.lower() == "e":
+            print("Direction: " + direction)
+            room = "33"
+        elif direction.lower() == "w":
+            print("Direction: " + direction)
+            room = "13"
+        else:
+            print("Not a valid direction!")
+    elif room == "33":
+        direction = input("You can travel: (W)est or (S)outh ")
+        if direction.lower() == "w":
+            print("Direction: " + direction)
+            room = "23"
+        elif direction.lower() == "s":
+            print("Direction: " + direction)
+            room = "32"
+        else:
+            print("Not a valid direction!")
+    elif room == "32":
+        direction = input("You can travel: (N)orth or (S)outh ")
+        if direction.lower() == "n":
+            print("Direction: " + direction)
+            room = "33"
+        elif direction.lower() == "s":
+            print("Direction: " + direction)
+            room = "31"
+        else:
+            print("Not a valid direction!")
+            
+print("Victory!")
