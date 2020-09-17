f1dba3e256896b279a35af8efc49bd04a775bac5
AtliSteinar <atlia20@ru.is>
2020-09-17 11:02:36 +0000

    Saving code

--- TileTraveller.py
+++ TileTraveller.py
@@ -2,49 +2,45 @@ room = "11"
 direction = ""
 while room != "31":
     if room == "11":
-        direction = input("You can travel: (N)orth. ")
+        print("You can travel: (N)orth.")
+        direction = input("Direction: ")
         if direction.lower() == "n":
-            print("Direction: " + direction)
             room = "12"
         else:
             print("Not a valid direction!")
     elif room == "12":
-        direction = input("You can travel: (N)orth or (E)ast or (S)outh. ")
+        print("You can travel: (N)orth or (E)ast or (S)outh. ")
+        direction = input("Direction: ")
         if direction.lower() == "n":
-            print("Direction: " + direction)
             room = "13"
         elif direction.lower() == "e":
-            print("Direction: " + direction)
             room = "22"
         elif direction.lower() == "s":
-            print("Direction: " + direction)
             room = "11"
         else:
             print("Not a valid direction!")
     elif room == "13":
-        direction = input("You can travel: (E)ast or (S)outh. ")
+        print("You can travel: (E)ast or (S)outh. ")
+        direction = input("Direction: ")
         if direction.lower() == "e":
-            print("Direction: " + direction)
             room = "13"
         elif direction.lower() == "s":
-            print("Direction: " + direction)
             room = "11"
         else:
             print("Not a valid direction!")
     elif room == "21":
-        direction = input("You can travel: (N)orth. ")
+        print("You can travel: (N)orth. ")
+        direction = input("Direction: ")
         if direction.lower() == "n":
-            print("Direction: " + direction)
             room = "22"
         else:
             print("Not a valid direction!")
     elif room == "22":
-        direction = input("You can travel: (S)outh or (W)est. ")
+        print("You can travel: (S)outh or (W)est. ")
+        direction = input("Direction: ")
         if direction.lower() == "s":
-            print("Direction: " + direction)
             room = "21"
         elif direction.lower() == "w":
-            print("Direction " + direction)
             room = "12"
         else:
             print("Not a valid direction!")
