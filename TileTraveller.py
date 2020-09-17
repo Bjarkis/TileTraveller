72a4887dea531d3f3630a254fa05a264530b5f20
AtliSteinar <atlia20@ru.is>
2020-09-17 10:43:38 +0000

    Saving code

--- TileTraveller.py
+++ TileTraveller.py
@@ -21,3 +21,30 @@ while room != "31":
             room = "11"
         else:
             print("Not a valid direction!")
+    elif room == "13":
+        direction = input("You can travel: (E)ast or (S)outh. ")
+        if direction.lower() == "e":
+            print("Direction: " + direction)
+            room = "13"
+        elif direction.lower() == "s":
+            print("Direction: " + direction)
+            room = "11"
+        else:
+            print("Not a valid direction!")
+    elif room == "21":
+        direction = input("You can travel: (N)orth. ")
+        if direction.lower() == "n":
+            print("Direction: " + direction)
+            room = "22"
+        else:
+            print("Not a valid direction!")
+    elif room == "22":
+        direction = input("You can travel: (S)outh or (W)est. ")
+        if direction.lower() == "s":
+            print("Direction: " + direction)
+            room = "21"
+        elif direction.lower() == "w":
+            print("Direction " + direction)
+            room = "12"
+        else:
+            print("Not a valid direction!")
